438bd09ae8440346dd4baafc9fbda21cff6d73c6
Yuki2no <eragbonikpeya@gmail.com>
2024-02-23 12:45:04 +0100

    changed API

--- script.js
+++ script.js
@@ -7,25 +7,26 @@ find.addEventListener('click', async()=> {
     //get the value of the location that will be seent to the server
     const location = search.value;
 
-    const url = `https://open-weather13.p.rapidapi.com/city/${location}`;
-    const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '21ed8aea1bmshe75ea1d8431ff22p18b244jsn0b0eb90eee3a',
-		'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
-	}
-};
+	const url = `https://the-weather-api.p.rapidapi.com/api/weather/${location}`;
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': '21ed8aea1bmshe75ea1d8431ff22p18b244jsn0b0eb90eee3a',
+			'X-RapidAPI-Host': 'the-weather-api.p.rapidapi.com'
+		}
+	};
 
 try {
 	const response = await fetch(url, options);
 	const result = await response.json();
 
 	//unknown or invalid loaction 
-	if(result.main === undefined){
+	if(result.data === undefined){
 		temp.innerHTML = "Location Not Found"
 		temp.style.fontSize = '20px'
 	} else{
-	temp.innerHTML =  Math.floor((result.main.temp - 32) * (5/9)) + "<sup>°c</sup>";
+	temp.innerHTML =  result.data.temp + "<sup>°c</sup>";
+	temp.style.fontSize = '5rem'
 	}
 
 } 
